2db685bff0103769abc687d90fa5c5f6ed76d356
Mazen <mazenkamal212@gmail.com>
2025-10-26 00:46:23 +0300

    Initialized the backend

---
+++ backend/src/main/java/com/sprintly/backend/HomeController.java
@@ -0,0 +1,20 @@
+package com.sprintly.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to the Sprintly Backend!";
+    }
+
+    @PostMapping("/")
+    public String postindex() {
+        return "This is a POST request to the Sprintly Backend!";
+    }
+}
