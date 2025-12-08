8a9609d20b074d9b84ca1a6974152e12305e05bb
Marwan <marwanay2005@gmail.com>
2025-12-09 00:45:28 +0200

    Created auth-pages

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component'; // check this path
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: AuthComponent },
+  { path: 'signup', component: AuthComponent },
+];
