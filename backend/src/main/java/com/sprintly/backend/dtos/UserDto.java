b4d947ef324c3baffde00738dcd91e67ff31d7a7
Mazen <mazenkamal212@gmail.com>
2025-12-21 22:40:15 +0200

    Fix build error

--- backend/src/main/java/com/sprintly/backend/dtos/UserDto.java
+++ backend/src/main/java/com/sprintly/backend/dtos/UserDto.java
@@ -2,9 +2,11 @@ package com.sprintly.backend.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
 public class UserDto {
     
     private Long id;
