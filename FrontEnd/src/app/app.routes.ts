725e257273011c2fe698f08e7e7a6612ee170dbb
Shehab Hegazy <shehab.sayed2005@gmail.com>
2025-12-24 01:46:03 +0200

    Create About Us page

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'login', component: AuthComponent },
   { path: 'signup', component: AuthComponent },
   { path: 'product/:id', component: ProductDetailsComponent },
+{ path: 'about', component: AboutComponent },
 ];
